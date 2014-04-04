d3b67a13340998c6a36f09dff38a3d96c6ca942f
RaelZero <gaz.gong@gmail.com>
2014-04-04 19:14:26 +0200

    Added escape if there are connection problems on random.lua

--- random.lua
+++ random.lua
@@ -3,13 +3,17 @@ local http = require("socket.http")
 -- checks the RANDOM.ORG quota for the current IP
 function checkQuota()
     -- get current IP
-    ip = http.request("http://curlmyip.com/")
+    ip , errcode = http.request("http://curlmyip.com/")
     ip = string.sub(ip, 1, -2)
+    
+    -- if return code is not 200, don't even bother trying
+    errcode = tonumber(errcode)
+    if (errcode ~= 200) then return -1 end
     
     -- factor URL
     url = "http://www.random.org/quota/?ip=" .. ip .. "&format=plain"
     
-    -- get quota and 
+    -- get quota from RANDOM.ORG, refactor the string and return
     quota = http.request(url)
     quota = tonumber(quota)
     print(quota)
@@ -21,9 +25,8 @@ function pullRandomBits(number, minv, maxv)
     currentQuota = checkQuota()
     
     -- only run if there is enough quota to satisfy the request
-    if (currentQuota >= number*8) then
-        -- initializes the randoms array
-        
+    if (currentQuota == -1) then return -1
+    elseif (currentQuota >= number*8) then
         -- creates the URL
         url = "http://www.random.org/integers/?num=" .. number .. "&min=" .. minv .. "&max=" .. maxv .. "&col=1&base=10&format=plain&rnd=new"
         
@@ -35,7 +38,6 @@ function pullRandomBits(number, minv, maxv)
         io.write(randoms)
         io.close()  
     end
-    
 end
 
 function debug()
